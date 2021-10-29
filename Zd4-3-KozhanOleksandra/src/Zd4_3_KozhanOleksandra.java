8ad5804c32eae3442b81b390f346d0402d15d6ea
AlexaKozhan555 <92484545+AlexaKozhan555@users.noreply.github.com>
2021-10-30 00:05:38 +0300

    Add files via upload

---
+++ Zd4-3-KozhanOleksandra/src/Zd4_3_KozhanOleksandra.java
@@ -0,0 +1,13 @@
+public class Zd4_3_KozhanOleksandra {
+    public static void main(String []args){
+            int suma_KozhanOleksandra = 0;
+            for(int i_KozhanOleksandra = 0; i_KozhanOleksandra <= 100; i_KozhanOleksandra++) {
+                if (i_KozhanOleksandra%2==0){
+                    suma_KozhanOleksandra+=i_KozhanOleksandra;
+                    System.out.println(suma_KozhanOleksandra);
+                }
+            }
+            System.out.println(suma_KozhanOleksandra);
+        }
+    }
+
